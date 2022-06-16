45945f0f3275be23d7cdffd8edac037e3f51a4fd
kkh9172@naver.com <72366153+KyungHoon416@users.noreply.github.com>
2022-06-16 09:06:59 +0900

    Initial Commit

---
+++ GItpush/GItpushApp.swift
@@ -0,0 +1,17 @@
+//
+//  GItpushApp.swift
+//  GItpush
+//
+//  Created by Inje Cho on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct GItpushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
